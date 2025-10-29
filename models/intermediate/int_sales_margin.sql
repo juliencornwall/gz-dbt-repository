abeb56b55c770300e57fdb7955062844939d87da
Julien Cornwall <julien.cornwall@gmail.com>
2025-10-29 14:38:21 +0000

    Joined Product & Sales for Purchase Cost & Margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,16 @@
+WITH product_sales_join AS 
+(
+SELECT
+products_id,
+SUM(revenue) AS revenue,
+SUM(quantity) AS quantity,
+SUM(quantity * purchase_price) AS purchase_cost
+FROM {{ ref('stg_raw__sales') }}
+LEFT JOIN {{ ref('stg_raw__product') }}
+USING (products_id)
+GROUP BY products_id
+)
+SELECT
+*
+, revenue - purchase_cost AS margin
+FROM product_sales_join
